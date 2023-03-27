ea574ba0d6ad8e2e4f8c7d74a054160e22408fed
kushal-dreamspring <kkhare@dreamspring.org>
2023-03-27 16:10:28 +0530

    Add acceptance specs
    - Add pending status to all specs

---
+++ spec/acceptance/parking_lot_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+# config.warnings = false
+module ParkingLot
+  RSpec.describe 'Parking' do
+    it 'should park a car' do
+      pending 'Not yet implemented'
+      response = ''
+      expect(response).to eq('Car Parked Successfully')
+    end
+
+    it 'should find the car parked' do
+      pending 'Not yet implemented'
+      response = ''
+      expect(response).to eq('Invalid Car Registration Number')
+    end
+
+    it 'list all the cars in the parking lot' do
+      pending 'Not yet implemented'
+      response = ''
+      expect(response).to eq('Slot already occupied')
+    end
+  end
+
+  RSpec.describe 'Invoice' do
+    it 'should generate an invoice when the car unparked' do
+      pending 'Not yet implemented'
+      response = ''
+      expect(response).to eq('Invalid Car Registration Number')
+    end
+
+    it 'list all the invoices' do
+      pending 'Not yet implemented'
+      response = ''
+      expect(response).to eq('Slot already occupied')
+    end
+
+    it 'display the invoice details' do
+      pending 'Not yet implemented'
+      response = ''
+      expect(response).to eq('Slot already occupied')
+    end
+  end
+end
