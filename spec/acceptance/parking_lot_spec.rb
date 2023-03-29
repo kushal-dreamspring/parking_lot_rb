b0a4dcf829f86f510b3e635abfabce340679dfe9
kushal-dreamspring <kkhare@dreamspring.org>
2023-03-29 13:46:33 +0530

    Move all imports to acceptance specs

--- spec/acceptance/parking_lot_spec.rb
+++ spec/acceptance/parking_lot_spec.rb
@@ -1,44 +1,46 @@
 # frozen_string_literal: true
 
-# config.warnings = false
-module ParkingLot
-  RSpec.describe 'Parking' do
-    it 'should park a car' do
-      pending 'Not yet implemented'
-      response = ''
-      expect(response).to eq('Car Parked Successfully')
-    end
+require 'time'
+require 'sequel'
 
-    it 'should find the car parked' do
-      pending 'Not yet implemented'
-      response = ''
-      expect(response).to eq('Invalid Car Registration Number')
-    end
+DATABASE = Sequel.connect('postgres://localhost:5432/test')
 
-    it 'list all the cars in the parking lot' do
-      pending 'Not yet implemented'
-      response = ''
-      expect(response).to eq('Slot already occupied')
-    end
+RSpec.describe 'Parking' do
+  it 'should park a car' do
+    pending 'Not yet implemented'
+    response = ''
+    expect(response).to eq('Car Parked Successfully')
   end
 
-  RSpec.describe 'Invoice' do
-    it 'should generate an invoice when the car unparked' do
-      pending 'Not yet implemented'
-      response = ''
-      expect(response).to eq('Invalid Car Registration Number')
-    end
+  it 'should find the car parked' do
+    pending 'Not yet implemented'
+    response = ''
+    expect(response).to eq('Invalid Car Registration Number')
+  end
 
-    it 'list all the invoices' do
-      pending 'Not yet implemented'
-      response = ''
-      expect(response).to eq('Slot already occupied')
-    end
+  it 'list all the cars in the parking lot' do
+    pending 'Not yet implemented'
+    response = ''
+    expect(response).to eq('Slot already occupied')
+  end
+end
 
-    it 'display the invoice details' do
-      pending 'Not yet implemented'
-      response = ''
-      expect(response).to eq('Slot already occupied')
-    end
+RSpec.describe 'Invoice' do
+  it 'should generate an invoice when the car unparked' do
+    pending 'Not yet implemented'
+    response = ''
+    expect(response).to eq('Invalid Car Registration Number')
+  end
+
+  it 'list all the invoices' do
+    pending 'Not yet implemented'
+    response = ''
+    expect(response).to eq('Slot already occupied')
+  end
+
+  it 'display the invoice details' do
+    pending 'Not yet implemented'
+    response = ''
+    expect(response).to eq('Slot already occupied')
   end
 end
