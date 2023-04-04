aac3e7f4ea388d81cd1a8c44b72f8de6cabf5900
kushal-dreamspring <kkhare@dreamspring.org>
2023-04-04 17:56:00 +0530

    Fix all-parked cars returning registration number

--- controller/all_cars.rb
+++ controller/all_cars.rb
@@ -3,6 +3,6 @@
 # Controller module for Parking Lot
 module ParkingLot
   def self.all_parked_cars
-    DATABASE[:slots].exclude(car_id: nil).all
+    Slot.exclude(car_id: nil).join(:cars, id: :car_id).all
   end
 end
