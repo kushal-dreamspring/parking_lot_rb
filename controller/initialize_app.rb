895f6d1475b2f931a0878d340bd4cd51c167dc32
kushal-dreamspring <kkhare@dreamspring.org>
2023-04-04 17:55:26 +0530

    Add reset-db method

--- controller/initialize_app.rb
+++ controller/initialize_app.rb
@@ -9,4 +9,10 @@ module ParkingLot
       Slot.create
     end
   end
+
+  def self.reset_db
+    DATABASE[:slots].exclude(car_id: nil).update(car_id: nil, entry_time: nil)
+    DATABASE[:invoices].delete
+    DATABASE[:cars].delete
+  end
 end
